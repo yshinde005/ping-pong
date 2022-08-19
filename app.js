52aed9ceddb5f8d0bd6c7816e2704bd2fdf1bfe0
yshinde005 <yashshinde0510gmail.com>
2022-08-19 15:38:34 +0530

    removed sounds

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
 
 // load sounds
-let hit = new Audio();
-let wall = new Audio();
-let userScore = new Audio();
-let comScore = new Audio();
+// let hit = new Audio();
+// let wall = new Audio();
+// let userScore = new Audio();
+// let comScore = new Audio();
 
-hit.src = "sounds/hit.mp3";
-wall.src = "sounds/wall.mp3";
-comScore.src = "sounds/comScore.mp3";
-userScore.src = "sounds/userScore.mp3";
+// hit.src = "sounds/hit.mp3";
+// wall.src = "sounds/wall.mp3";
+// comScore.src = "sounds/comScore.mp3";
+// userScore.src = "sounds/userScore.mp3";
 
 // Ball object
 const ball = {
