b54370a7b831ee7134b5966fd96f9204efd4ee69
yshinde005 <yashshinde0510gmail.com>
2022-08-20 13:55:17 +0530

    new-commit

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ const canvas = document.getElementById("pong");
 // getContext of canvas = methods and properties to draw and do a lot of thing to the canvas
 const ctx = canvas.getContext("2d");
 
-// load sounds
-// let hit = new Audio();
-// let wall = new Audio();
-// let userScore = new Audio();
-// let comScore = new Audio();
-
-// hit.src = "sounds/hit.mp3";
-// wall.src = "sounds/wall.mp3";
-// comScore.src = "sounds/comScore.mp3";
-// userScore.src = "sounds/userScore.mp3";
-
 // Ball object
 const ball = {
   x: canvas.width / 2,
